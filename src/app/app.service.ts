54170f2726f57dab7a1b79a4917843ff42e4676d
William Nascimento <william.nascimento2010@gmail.com>
2025-09-25 23:23:38 -0300

    api url

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environments';
+import { AppConfigService } from './app-config.service';
 
 const URL_CALENDARS = '/api/v1/calendar';
 const URL_CLIENTS = '/api/v1/client';
@@ -15,55 +16,56 @@ const URL_SPECIFICATIONS = '/api/v1/specifications';
   providedIn: 'root'
 })
 export class AppService {
-  
-  constructor(private http: HttpClient){
+  private apiUrl: string;
 
+  constructor(private http: HttpClient, private appConfig: AppConfigService) {
+    this.apiUrl = appConfig.apiUrl;
   }
 
   save(calendar: any): Observable<any>{
-    return this.http.post(`${environment.URL_API}${URL_CALENDARS}`,calendar)
+    return this.http.post(`${this.apiUrl}${URL_CALENDARS}`,calendar)
     .pipe(map((resp: any) => {
       return resp;
     }));
   }
 
   update(calendar: any): Observable<any>{
-    return this.http.put(`${environment.URL_API}${URL_CALENDARS}/agendamento/${calendar.id}`,calendar)
+    return this.http.put(`${this.apiUrl}${URL_CALENDARS}/agendamento/${calendar.id}`,calendar)
     .pipe(map((resp: any) => {
       return resp;
     }));
   }
 
   getCalendarView(startDate: string, endDate: string): Observable<any>{
-    return this.http.get(`${environment.URL_API}${URL_CALENDARS}/view-agendamento?startDate=${startDate}&endDate=${endDate}`)
+    return this.http.get(`${this.apiUrl}${URL_CALENDARS}/view-agendamento?startDate=${startDate}&endDate=${endDate}`)
     .pipe(map((resp: any) => {
       return resp;
     }));
   }
 
   loadCalendarById(id: string){
-    return this.http.get(`${environment.URL_API}${URL_CALENDARS}/by-id?id=${id}`)
+    return this.http.get(`${this.apiUrl}${URL_CALENDARS}/by-id?id=${id}`)
     .pipe(map((resp: any) => {
       return resp;
     }));
   }
 
   getClients(ativo: boolean, search: string): Observable<any[]> {
-    return this.http.get(`${environment.URL_API}${URL_CLIENTS}/?ativo=${ativo}&search=${search}`)
+    return this.http.get(`${this.apiUrl}${URL_CLIENTS}/?ativo=${ativo}&search=${search}`)
     .pipe(map((resp: any) => {
       return resp;
     }));
   }
 
   loadEquipment(ativo: boolean): Observable<any[]> {
-    return this.http.get(`${environment.URL_API}${URL_EQUIPMENT}?ativo=${ativo}`)
+    return this.http.get(`${this.apiUrl}${URL_EQUIPMENT}?ativo=${ativo}`)
     .pipe(map((resp: any) => {
       return resp;
     }));
   }
 
   loadSpecifications(): Observable<any[]> {
-    return this.http.get(`${environment.URL_API}${URL_SPECIFICATIONS}`)
+    return this.http.get(`${this.apiUrl}${URL_SPECIFICATIONS}`)
     .pipe(map((resp: any) => {
       return resp;
     }));
@@ -72,14 +74,14 @@ export class AppService {
   
 
 //   save(calendar: Calendar): Observable<Calendar>{
-//     return this.http.post(`${environment.URL_API}${URL_CALENDARS}`,calendar)
+//     return this.http.post(`${this.apiUrl}${URL_CALENDARS}`,calendar)
 //     .pipe(map((resp: Calendar) => {
 //       return resp;
 //     }));
 //   }
 
 //   update(calendar: Calendar): Observable<Calendar>{
-//     return this.http.put(`${environment.URL_API}${URL_CALENDARS}/${calendar.id}`,calendar)
+//     return this.http.put(`${this.apiUrl}${URL_CALENDARS}/${calendar.id}`,calendar)
 //     .pipe(map((resp: Calendar) => {
 //       return resp;
 //     }));
