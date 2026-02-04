4f6858e083034b3e2985d580e8627d72d1cad261
danweb-admin <143641802+danweb-admin@users.noreply.github.com>
2026-02-03 23:27:21 -0300

    Update environments.prod.ts

--- src/environments/environments.prod.ts
+++ src/environments/environments.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  URL_API: 'https://backend.belacrio-agenda.online', 
+  URL_API: 'https://backend.agenda-an-locacoes.site', 
   // URL_API: 'http://95.111.226.231:32112',
   // URL_API: 'http://52.0.179.113:5000',
   hmr: false,
